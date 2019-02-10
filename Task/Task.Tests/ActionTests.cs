8fb8c197645d105d6825a5722a4fd9912b5324fc
Anonymous <Anonymous@Anonymous.Com>
2019-02-10 18:01:31 +0200

    New Tests Addeded.

--- Task/Task.Tests/ActionTests.cs
+++ Task/Task.Tests/ActionTests.cs
@@ -24,6 +24,32 @@ namespace Task.Tests
             Assert.AreEqual(expectedsecond,second);
         }
 
+        [Test]
+        public void Exchange_n88_and_42_42_and_n88returned()
+        {
+            int first = -88;
+            int second = 42;
+            int expectedfirst = 42;
+            int expectedsecond = -88;
 
+            Action action = new Action();
+            action.Exchange(ref first, ref second);
+
+            Assert.AreEqual(expectedfirst, first);
+            Assert.AreEqual(expectedsecond, second);
+        }
+
+        [Test]
+        public void FindMean_4_and_8_6_Expected()
+        {
+            int first = 4;
+            int second = 8;
+            int expected = 6;
+
+            Action action = new Action();
+            float actual = action.FindMean(first, second);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
