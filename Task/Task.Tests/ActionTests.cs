e31888fa2ca3f9a424f387334cd76251c2e713b7
Anonymous <Anonymous@Anonymous.Com>
2019-02-11 08:28:27 +0200

    CaseTest Addeded.

--- Task/Task.Tests/ActionTests.cs
+++ Task/Task.Tests/ActionTests.cs
@@ -10,61 +10,21 @@ namespace Task.Tests
         {
         }
 
-        [Test]
-        public void Exchange_4_and_8_8_and_4returned()
+        [TestCase(4,8)]
+        [TestCase(-88, 42)]
+        [TestCase(-88, 42)]
+        public void Exchange_Tests(int first, int second)
         {
-            var first = 4;
-            var second = 8;
-            var expectedfirst = 8;
-            var expectedsecond = 4;
+            var expectedFirst = second;
+            var expectedSecond = first;
 
             var action = new Action();
             action.Exchange(ref first, ref second);
 
-            Assert.AreEqual(expectedfirst, first);
-            Assert.AreEqual(expectedsecond,second);
+            Assert.AreEqual(expectedFirst, first);
+            Assert.AreEqual(expectedSecond,second);
         }
 
-        [Test]
-        public void Exchange_n88_and_42_42_and_n88returned()
-        {
-            var first = -88;
-            var second = 42;
-            var expectedfirst = 42;
-            var expectedsecond = -88;
-
-            var action = new Action();
-            action.Exchange(ref first, ref second);
-
-            Assert.AreEqual(expectedfirst, first);
-            Assert.AreEqual(expectedsecond, second);
-        }
-
-        [Test]
-        public void FindMean_4_and_8_6_Expected()
-        {
-            var first = 4;
-            var second = 8;
-            var expected = 6;
-
-            var action = new Action();
-            var actual = action.FindMean(first, second);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void FindMean_n88_and_42_n23_Expected()
-        {
-            var first = -88f;
-            var second = 42f;
-            var expected = -23f;
-
-            var action = new Action();
-            var actual = action.FindMean(first, second);
-
-            Assert.AreEqual(expected, actual);
-        }
     }
 
     public class RomanNumeralsTests
