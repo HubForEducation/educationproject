2bc13e0bda1919020f1b9f7a85248e35ca4eace2
Anonymous <Anonymous@Anonymous.Com>
2019-02-16 13:08:08 +0200

    Addeded New Project.

--- Geometry/Geometry/Geometry.cs
+++ Geometry/Geometry/Geometry.cs
@@ -1,27 +1,91 @@
+﻿using System;
+
 namespace Geometry
 {
     public class Geometry
     {
+        public char Brush { get; set; } = '*';
+        public char Font { get; set; } = '*';
+
         public void DrawSquare(int size)
         {
+            Console.Clear();
             if (size <= 0)
             {
                 throw new ArgumentOutOfRangeException("Only 1 and more!");
             }
             else
             {
-                var brush = "*";
-                var body = new String(brush, n);
-                Console.WriteLine(body);
+                var brush = '*';
+                var line = new String(Brush, size);
+                Console.WriteLine(line);
                 int i = 0;
                 while (i != size - 2)
                 {
-                    Console.WriteLine(brush + new string(' ', size - 2) + brush);
+                    Console.WriteLine(brush + new string(Font, size - 2) + Font + brush + Font);
                     i++;
                 }
-                Console.WriteLine(body);
+
+                Console.WriteLine(line);
                 Console.ReadKey();
             }
+        }
+
+        public void DrawTrangle(int size)
+        {
+            Console.Clear();
+            if (size < 1)
+            {
+                Console.WriteLine("Only 1 and more!");
+                Console.ReadKey();
+            }
+            else
+            {
+                int count = 1;
+                while (size-- != 0)
+                {
+                    int c = count;
+                    while (c-- != 0)
+                    {
+                        Console.Write(Brush);
+                    }
+
+                    Console.WriteLine();
+                    count = count + 2;
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+        public void DrawCircle(int size)
+        {
+            Console.Clear();
+            if (size < 1)
+            {
+                Console.WriteLine("Only 1 and more!");
+                Console.ReadKey();
+            }
+            else
+            {
+                void Write(int xp, int yp)
+                {
+                    Console.SetCursorPosition(xp, yp);
+                    Console.Write(Brush);
+                }
+
+                int centerX = size * 2, centerY = size, radius = size, x = -radius;
+                while (x < radius)
+                {
+                    var y = (int) Math.Floor(Math.Sqrt(radius * radius - x * x));
+
+                    Write(x + centerX, y + centerY);
+                    y = -y;
+                    Write(x + centerX, y + centerY);
+                    x++;
+                }
+
+                Console.ReadLine();
             }
         }
     }
--- Geometry/Geometry/Program.cs
+++ Geometry/Geometry/Program.cs
@@ -4,10 +4,34 @@ namespace Geometry
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var geometry = new Geometry();
-            geometry.DrawSquare(-4);
+            int number, size;
+            var drawing = new Geometry();
+            Console.WriteLine("Enter number of figure.");
+            Console.WriteLine("1. Square.");
+            Console.WriteLine("2. Triangle.");
+            Console.WriteLine("2. Square.");
+            number = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException("The value cannot be empty."));
+            Console.WriteLine("Okay, new enter size.");
+            size = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException("The value cannot be empty."));
+            if (number == 1)
+            {
+                drawing.DrawSquare(size);
+            }
+            else if (number == 2)
+            {
+                drawing.DrawSquare(size);
+            }
+            else if (number == 3)
+            {
+                drawing.DrawCircle(size);
+            }
+            else
+            {
+                Console.WriteLine("Invalid number of figure.");
+                Console.ReadKey();
+            }
         }
     }
 }
