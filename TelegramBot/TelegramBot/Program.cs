da1369c78c1bcfeb476653888311931866b61a11
HubForEducation <hubforeducation@yandex.ru>
2019-02-19 13:39:18 +0200

    SaveLoad.

--- TelegramBot/TelegramBot/Program.cs
+++ TelegramBot/TelegramBot/Program.cs
@@ -7,6 +7,7 @@ namespace TelegramBot
         static void Main()
         {
            BotEngine bot = new BotEngine();
+           bot.Get();
         }
     }
 }
--- TelegramBot/TelegramBot/Settings.cs
+++ TelegramBot/TelegramBot/Settings.cs
@@ -5,7 +5,7 @@ namespace TelegramBot
 {
     public static class Settings
     {
-        public static string Get { get; set; }
+        public static string Get { get; set;}
         public static string Read { get; set; }
         public static string DowloadAdress { get; set; }
         public static string DowloadPath { get; set; }
