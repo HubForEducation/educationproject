6f50a9eb02f72a4f0eac692e7cad2d4ab194afb4
Anonymous <Anonymous@Anonymous.Com>
2019-03-03 17:16:46 +0200

    //NEWARCH!

--- TelegramBot/TelegramBot/Program.cs
+++ TelegramBot/TelegramBot/Program.cs
@@ -25,7 +25,7 @@ namespace TelegramBot
             checkUpdateThread.Start();
 
             _botClient = new TelegramBotClient(ApiToken);
-            _botClient.OnMessage += Bot_Commands;
+            _botClient.OnMessage += BotCommands.GetCommands;
 
             _botClient.StartReceiving();
             Thread.Sleep(int.MaxValue);
@@ -106,87 +106,6 @@ namespace TelegramBot
                     Console.WriteLine("Wrong number.");
                 }
             } while (Keypressed != 3);
-        }
-
-        static async void Bot_Commands(object sender, MessageEventArgs e)
-        {
-            if (e.Message.Text == "/get")
-            {
-                var files = BotLogic.Get(_settings.Get);
-                await _botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: files
-                );
-            }
-
-            if (e.Message.Text.StartsWith("/get "))
-            {
-                var message = e.Message.Text.Substring(5);
-                var files = BotLogic.Get(message);
-                await _botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: files
-                );
-            }
-
-            else if (e.Message.Text == "/read")
-            {
-                var file = BotLogic.Read(_settings.Read);
-
-                await _botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: file
-                );
-            }
-
-            else if (e.Message.Text == "/download")
-            {
-                var downloadmessage = BotLogic.Download(_settings.DownloadAdress, _settings.DownloadPath);
-                await _botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: downloadmessage
-                );
-            }
-
-            else if (e.Message.Text == "/command")
-            {
-                var commandanswer = BotLogic.Command(_settings.Command);
-
-                await _botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: commandanswer
-                );
-            }
-            else if (e.Message.Text == "/check")
-            {
-                BotLogic.Check(_settings.CheckPath);
-                string checkedanswer = BotLogic.Checked(BotLogic.Checkedstring);
-                if (checkedanswer != null)
-                {
-                    await _botClient.SendTextMessageAsync(
-                        chatId: e.Message.Chat,
-                        text: checkedanswer
-                    );
-                }
-                else
-                {
-                    await _botClient.SendTextMessageAsync(
-                        chatId: e.Message.Chat,
-                        text: "No changes."
-                    );
-                }
-
-                BotLogic.Checkedstring = null;
-            }
-            else if (e.Message.Text == "/help")
-            {
-                var help = new Help();
-                
-                await _botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: help.Show()
-                );
-            }
         }
     }
 }
